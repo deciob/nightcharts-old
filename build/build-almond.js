d7be36d711f3e2fee7aa1e82035e24ddabd693c4
deciob <decio.battaglia@gmail.com>
2014-01-27 21:00:09 +0000

    almond distribution

---
+++ build/build-almond.js
@@ -0,0 +1,9 @@
+{
+  baseUrl: "../src/",
+  paths: {
+    d3: '../lib/d3/d3',
+    almond: '../lib/almond/almond'
+  },
+  exclude: ['d3'],
+  name: 'almond'
+}
--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,3 @@
 r.js -o build.js out=../dist/nightcharts.js optimize=none
+r.js -o build-almond.js out=../dist/nightcharts-wrap.js optimize=none wrap=true
 docco --output ../docs ../dist/nightcharts.js
