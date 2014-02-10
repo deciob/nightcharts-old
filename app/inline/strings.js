ae660d5747589155aaf557f8d2f7f9c792ff4075
deciob <decio.battaglia@gmail.com>
2014-02-10 09:39:00 +0000

    small text change on sparkline example

--- app/inline/strings.js
+++ app/inline/strings.js
@@ -1,3 +1,3 @@
 define({
-  start_statement: 'In 1950 the urban world population was 745,495 million; by 2015 it will reach 3,926,793 million, beating the 1950 statistic 5 fold'
+  start_statement: 'In 1950 the urban world population was 745,495 million; by 2015 it will reach 3,926,793 million, beating the 1950 statistic 5 fold:'
 });
